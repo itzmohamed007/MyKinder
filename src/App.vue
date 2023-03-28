8ff5f0af2c96f7abf6b82d6c64fd5e5f9057a7c7
itzmohamed007 <itzmohamed007@gmail.com>
2023-03-28 17:25:06 +0000

    init

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav class="navbar navbar-expand-lg fixed-top" id="mainNav">
+    <div class="container px-4 px-lg-5">
+      <span class="navbar-brand fw-bold">MyVisa</span>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse"
+        data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+        aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarResponsive">
+          <ul class="navbar-nav fw-bold align-items-center">
+            <router-link class="nav-item rounded" to="/">Home</router-link>
+            <router-link class="nav-item rounded" to="/">About</router-link>
+            <router-link class="nav-item rounded" to="/">Contact</router-link>
+            <router-link class="login nav-item rounded" to="/">Login</router-link>
+          </ul>
+        </div>
+      </div>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  font-family: 'Poppins', sans-serif;
+}
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+nav ul a {
+  text-decoration: none;
+  color: black;
+  margin-inline: 5px 20px;
+}
+nav ul .login {
+  border: 3px solid black;
+  padding: 5px 20px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { Icon } from "@iconify/vue";
+</script>
+
+<template>
+  <section class="hero">
+    <div class="form container rounded d-flex flex-column align-items-center justify-content-center">
+      <h1 class="fs-2 fw-bold">Login</h1>
+      <div class="input-container w-50 fs-6">
+        <input class="input-field border-0" type="email" placeholder="Email" />
+        <Icon class="mx-2" icon="material-symbols:mail-rounded" color="black" width="30" height="30"/>
+        <hr>
+      </div>
+      <div class="input-container w-50 fs-6">
+        <input class="input-field border-0" type="password" placeholder="Password"/>
+        <Icon class="mx-2" icon="material-symbols:lock" color="black" width="30" height="30"/>
+      </div>
+      <button class="btn bg-dark text-white">Login</button>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.hero {
+  padding: 5rem;
+  height: 100vh;
+  background: url("../assets/background.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.form {
+  /* padding: 5rem 0; */
+  opacity: 50;
+  background-color: #fff;
+}
+</style>
